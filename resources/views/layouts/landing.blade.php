ce28ad5feee9cc6750bb771445e3a2d28e4ac553
Amonel Leais <96805614+Amoneleais@users.noreply.github.com>
2025-08-29 01:09:30 -0300

    feat: add landing page layout with hero section, features, testimonials, and call to action

---
+++ resources/views/layouts/landing.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <title>Gestão Empresarial Moderna</title>
+  </head>
+  <body>
+    <main>
+      @yield('content')
+    </main>
+  </body>
+</html>
